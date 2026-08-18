6fcafde77a7cdf153608b3f689f3bb5ab39e381e
Pedro_Fusco <Pedropfusco@gmail.com>
2026-08-18 17:11:58 -0300

    Add Firebase configuration for Honeymoon-2026 project

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Projeto: Honeymoon-2026 (honeymoon-2026-bupe)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCe4nziLU7Ihh6nyLfVETm-JYROP9EEclI",
+  authDomain: "honeymoon-2026-bupe.firebaseapp.com",
+  databaseURL: "https://honeymoon-2026-bupe-default-rtdb.firebaseio.com",
+  projectId: "honeymoon-2026-bupe",
+  storageBucket: "honeymoon-2026-bupe.firebasestorage.app",
+  messagingSenderId: "460028716679",
+  appId: "1:460028716679:web:9dbc54d11851131db07a22"
+};
+
+export const configurado = !JSON.stringify(firebaseConfig).includes("COLE_AQUI");  
